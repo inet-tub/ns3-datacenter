22a9ab7d15c1d91543077ecab2a6ab13347cac45
vamsiDT <f2013790@goa.bits-pilani.ac.in>
2022-05-23 22:00:32 +0000

    removing unwanted print statements

--- simulator/ns-3.35/src/applications/model/packet-sink.cc
+++ simulator/ns-3.35/src/applications/model/packet-sink.cc
@@ -287,7 +287,7 @@ void PacketSink::HandleRead (Ptr<Socket> socket)
           }
           else{
             m_flowFinishTrace(totalSize, m_startTime.GetNanoSeconds(),false,sender_priority);
-            std::cout << "Flow finished. FCT = " << Simulator::Now().GetSeconds()-m_startTime.GetSeconds() << " seconds" << std::endl;
+            // std::cout << "Flow finished. FCT = " << Simulator::Now().GetSeconds()-m_startTime.GetSeconds() << " seconds" << std::endl;
           }
           StopApplication();
         }
