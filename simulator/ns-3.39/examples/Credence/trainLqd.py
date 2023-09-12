e42b1ad9838867c67af7bf130d16528f64c7ea32
vamsiDT <f2013790@goa.bits-pilani.ac.in>
2023-09-12 21:25:56 +0000

    minor

--- simulator/ns-3.39/examples/Credence/trainLqd.py
+++ simulator/ns-3.39/examples/Credence/trainLqd.py
@@ -9,7 +9,7 @@ Created on Sat Sep  9 15:50:18 2023
 import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, ConfusionMatrixDisplay
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, f1_score, ConfusionMatrixDisplay
 from sklearn.model_selection import RandomizedSearchCV, train_test_split
 from scipy.stats import randint
 from sklearn.tree import export_graphviz
@@ -24,8 +24,9 @@ csvfile=str(sys.argv[1])
 dumpfile=str(sys.argv[2])
 # dumpfile="/home/vamsi/lakewood/src/phd/codebase/ns3-datacenter-Old/simulator/ns-3.35/examples/Credence/rf_models/model-2-0.8-0.875-2-WS-0.joblib"
 maxDepth=int(sys.argv[3])
+switchId=str(sys.argv[4])
 # maxDepth=4
-numTrees=int(sys.argv[4])
+# numTrees=int(sys.argv[4])
 # numTrees=1
 #%%
 data = pd.read_csv(csvfile,delimiter=' ',usecols=[0,1,2,3,4])
@@ -35,10 +36,43 @@ y = data['drop'].values
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.6)
 
-rf = RandomForestClassifier(max_depth=maxDepth, n_jobs=-1,n_estimators=numTrees)
-rf.fit(X_train, y_train)
+trees=[1,2,4,8,16,32,64,128]
+for numTrees in trees:
+    rf = RandomForestClassifier(max_depth=maxDepth, n_jobs=-1,n_estimators=numTrees)
+    rf.fit(X_train, y_train)
 
-y_pred = rf.predict(X_test)
+    y_pred = rf.predict(X_test)
+
+    try:
+        accuracy = accuracy_score(y_test, y_pred)
+    except:
+        accuracy = 1
+    if (accuracy==0):
+        accuracy = 1
+    try:
+        precision = precision_score(y_test, y_pred)
+    except:
+        precision = 1
+    if (precision==0):
+        precision = 1
+    try:
+        recall = recall_score(y_test, y_pred)
+    except:
+        recall = 1
+    if (recall==0):
+        recall = 1
+    try:
+        f1score = f1_score(y_test,y_pred)
+    except:
+        f1score = 1
+    if (f1score==0):
+        f1score = 1
+    print(accuracy,precision,recall,f1score,numTrees,maxDepth)
+
+    joblib.dump(rf,dumpfile+'-'+str(numTrees)+'-'+switchId+'.joblib')
+
+
+
 #%%
 # feature_importances = pd.Series(rf.feature_importances_, index=["queueLength","sharedOccupancy", "averageQueueLength","averageOccupancy"]).sort_values(ascending=False)
 # feature_importances.plot.bar();
@@ -57,30 +91,6 @@ y_pred = rf.predict(X_test)
 # cm = confusion_matrix(y_test, y_pred)
 # ConfusionMatrixDisplay(confusion_matrix=cm).plot();
 #%%
-try:
-    accuracy = accuracy_score(y_test, y_pred)
-except:
-    accuracy = 1
-if (accuracy==0):
-    accuracy = 1
-try:
-    precision = precision_score(y_test, y_pred)
-except:
-    precision = 1
-if (precision==0):
-    precision = 1
-try:
-    recall = recall_score(y_test, y_pred)
-except:
-    recall = 1
-if (recall==0):
-    recall = 1
-
-print("Accuracy:", accuracy)
-print("Precision:", precision)
-print("Recall:", recall)
-#%%
-joblib.dump(rf,dumpfile)
 
 
 #%%
