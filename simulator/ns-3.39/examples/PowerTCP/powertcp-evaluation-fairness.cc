ca9de8130ba9345ef533d957836c74fd7e0104a7
vamsiDT <f2013790@goa.bits-pilani.ac.in>
2023-11-01 16:22:15 +0100

    setting default node type to zero explicitly

--- simulator/ns-3.39/examples/PowerTCP/powertcp-evaluation-fairness.cc
+++ simulator/ns-3.39/examples/PowerTCP/powertcp-evaluation-fairness.cc
@@ -397,7 +397,7 @@ int main(int argc, char *argv[])
 
 		uint32_t algorithm=3;
 		uint32_t windowCheck=1;
-		std::string confFile = "/home/vamsi/src/phd/ns3-datacenter/simulator/ns-3.35/examples/PowerTCP/config-fairness.txt";
+		std::string confFile = "/home/vamsi/src/phd/codebase/ns3-datacenter/simulator/ns-3.39/examples/PowerTCP/config-fairness.txt";
 
 		std::cout << confFile;
 		CommandLine cmd;
--- simulator/ns-3.39/examples/PowerTCP/powertcp-evaluation-workload.cc
+++ simulator/ns-3.39/examples/PowerTCP/powertcp-evaluation-workload.cc
@@ -574,8 +574,8 @@ int main(int argc, char *argv[])
 	uint32_t algorithm = 3;
 	uint32_t windowCheck = 1;
 
-	std::string confFile = "/home/vamsi/src/phd/ns3-datacenter/simulator/ns-3.35/examples/PowerTCP/config-workload.txt";
-	std::string cdfFileName = "/home/vamsi/src/phd/ns3-datacenter/simulator/ns-3.35/examples/PowerTCP/websearch.txt";
+	std::string confFile = "/home/vamsi/src/phd/codebase/ns3-datacenter/simulator/ns-3.39/examples/PowerTCP/config-workload.txt";
+	std::string cdfFileName = "/home/vamsi/src/phd/codebase/ns3-datacenter/simulator/ns-3.39/examples/PowerTCP/websearch.txt";
 
 	unsigned randomSeed = 7;
 
